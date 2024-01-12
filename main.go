a830414fb764384d0e53f894e86a65ce824fb058
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-12 19:52:39 +0530

    ADD: Library Godotenv

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
+	godotenv.Load()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("Port not found in the environment")
