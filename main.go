998c2c936ead0435e9a54e188773f6624fc92311
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-12 19:14:55 +0530

    ADD: port value

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port not found in the environment")
+	}
+
+	fmt.Printf("Project is working on Port %s \n", port)
+
 	return
 }
