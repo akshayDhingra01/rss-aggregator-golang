ff493f4b596c788f866aa74dee533f0e579143d7
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-13 19:43:48 +0530

    ADD: handler for http request

---
+++ handler_readiness.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {// define a http handler as go library expects 
+	respondWithJson(w, 200, struct{}{})
+
+}
