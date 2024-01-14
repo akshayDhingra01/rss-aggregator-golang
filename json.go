df8da00d98e867f2f2c1b888ade1d8d8e6b76872
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-14 19:50:41 +0530

    ADD: Respond with Error function

--- json.go
+++ json.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func respondWithError( w http.ResponseWriter, code int, msg string)  { // http handlers use this http.ResposeWriter
+	if code > 499 {
+		log.Println("Responding with Error 5xx:", msg)
+	}
+	type errorResponse struct {
+		Error string `json: "error"` // json error is used to write so to say that in key-value pair, value of key should be error
+	}
+	respondWithJson(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
 func respondWithJson( w http.ResponseWriter, code int, payload interface{})  { // http handlers use this http.ResposeWriter
 	data, err := json.Marshal(payload)
 	if err!= nil {
